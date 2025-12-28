b392f4369de021b6f58b193525e83798dc3a35bc
Geiger Games <dominikkarolcik@gmail.com>
2025-12-28 10:33:47 +0000

    docwt change

--- sbin/docwt.c
+++ sbin/docwt.c
@@ -14,29 +14,39 @@ int main(int argc, char *argv[]) {
 	puts(":s to save your file, :c to copy/continue,");
 	puts("or :q to quit.");
 
-	FILE *f = fopen(argv[1], "r");
-	if (!f) {
-		return 27; // file fail
-	}
-
-	int lines = 0;
-
-	char **filebuf = malloc(lines * sizeof(char *));
-	if (!filebuf) {return 54;}
+	int lines;
+	char **filebuf;
 
 	char l[1024] = {0};
 
-	// allocate each line
-	while (fgets(l, 1023, f)) {
-		filebuf[lines] = malloc(1024);
-		if (!filebuf[lines]) {return 54;}
-		strncpy(filebuf[lines], l, 1023);
-		lines++;
+	FILE *f = fopen(argv[1], "r");
+	if (!f) {
+		printf("%s is a new file\n", argv[1]);
+
+		lines = 1;
+
+		filebuf = malloc(lines * sizeof(char *));
+
+		filebuf[0] = malloc(1024);
+		if (!filebuf[0]) {return 54; /*alloc fail*/}
+	} else {
+		lines = 0;
+
+		filebuf = malloc(lines * sizeof(char *));
+		if (!filebuf) {return 54;}
+
+		// allocate each line
+		while (fgets(l, 1023, f)) {
+			filebuf[lines] = malloc(1024);
+			if (!filebuf[lines]) {return 54;/*alloc fail*/}
+			strncpy(filebuf[lines], l, 1023);
+			lines++;
+		}
+
+		printf("Previously %d lines in %s\n", lines, argv[1]);
+
+		fclose(f);
 	}
-
-	printf("Previously %d lines in %s\n", lines, argv[1]);
-
-	fclose(f);
 
 	f = fopen(argv[1], "w");
 	if (!f) {
@@ -58,13 +68,14 @@ int main(int argc, char *argv[]) {
 
 		if (l[0] == ':' && l[1] == 'l') {
 			char *num = l + 2;
-			currentLine = atoi(num);
+			currentLine = atoi(num) - 1;
 			continue;
 		} else if (l[0] == ':' && l[1] == 's') {
 			for (int i = 0; i < lines; i++) {fputs(filebuf[i], f);}
 			continue;
 		} else if (l[0] == ':' && l[1] == 'q') {
 			fclose(f);
+			free(filebuf);
 			return 0;
 		} else if (l[0] == ':' && l[1] == 'c') {
 			currentLine++;
