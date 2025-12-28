d070fe043de50ab36d93c6b974b26f529bdcfe40
Geiger Games <dominikkarolcik@gmail.com>
2025-12-28 10:58:42 +0000

    megacompiler

---
+++ compile.sh
@@ -0,0 +1,9 @@
+gcc ./main.c -o ./termOS
+
+gcc ./sbin/calc.c -o ./sbin/calc
+gcc ./sbin/docrd.c -o ./sbin/docrd
+gcc ./sbin/docwt.c -o ./sbin/docwt
+gcc ./sbin/help.c -o ./sbin/help
+gcc ./sbin/listd.c -o ./sbin/listd
+gcc ./sbin/shell.c -o ./sbin/shell
+gcc ./sbin/stats.c -o ./sbin/stats
